c3ee294e15100c263506d9b687a2c533be05eb68
feden2906 <feden2906@gmail.com>
2021-10-19 21:17:22 +0300

    fix

--- configs/config.js
+++ configs/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    MONGO_CONNECT_URL: process.env.MONGO_CONNECT_URR || 'mongodb://localhost:27017/my-db',
-    PORT: process.env.PORT || 5000,
+  MONGO_CONNECT_URL: process.env.MONGO_CONNECT_URR || 'mongodb://localhost:27017/my-db',
+  PORT: process.env.PORT || 5000,
 
-    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET || 'xxx',
-    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || 'zzz',
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET || 'xxx',
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || 'zzz',
 
-    NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL,
-    NO_REPLY_EMAIL_PASSWORD: process.env.NO_REPLY_EMAIL_PASSWORD
+  NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL || "maksona2906@gmail.com",
+  NO_REPLY_EMAIL_PASSWORD: process.env.NO_REPLY_EMAIL_PASSWORD || "ozater12345"
 };
--- services/email.service.js
+++ services/email.service.js
@@ -1,25 +1,41 @@
 const nodemailer = require('nodemailer');
-const EmailTemplates = require('emailTemplates')
+const EmailTemplates = require('email-templates');
+const path = require('path')
 
-const {NO_REPLY_EMAIL_PASSWORD, NO_REPLY_EMAIL} = require('../configs/config');
+const { NO_REPLY_EMAIL_PASSWORD, NO_REPLY_EMAIL } = require('../configs/config');
+const allTemplates = require('../emailTemplates');
+
+const templateParser = new EmailTemplates({
+  views: {
+    root: path.join(process.cwd(), 'emailTemplates')
+  }
+})
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: NO_REPLY_EMAIL,
-        pass: NO_REPLY_EMAIL_PASSWORD
-    }
+  service: 'gmail',
+  auth: {
+    user: NO_REPLY_EMAIL,
+    pass: NO_REPLY_EMAIL_PASSWORD
+  }
 });
 
-const sendMail = async (userMail) => {
-    return transporter.sendMail({
-        from: 'Name',
-        to: userMail,
-        subject: 'Theme of mail',
-        html: 'Hello world'
-    })
+const sendMail = async (userMail, emailAction, context = {}) => {
+
+  const templateInfo = allTemplates[emailAction];
+
+  if (!templateInfo) {
+    throw new Error('TEmplates liagli');
+  }
+  const html = await templateParser.render(templateInfo.templateName, context);
+
+  return transporter.sendMail({
+    from: 'Name',
+    to: userMail,
+    subject: templateInfo.subject,
+    html
+  })
 }
 
 module.exports = {
-    sendMail
+  sendMail
 }
