50130324cfc7c4c43ddcf37823e37b7904ecdf2e
sergey23131 <semno321@gmail.com>
2021-10-26 21:38:27 +0300

    node 10

--- services/user.service.js
+++ services/user.service.js
@@ -2,6 +2,31 @@ const User = require('../database/User');
 
 module.exports = {
     getAllUsers: (query = {}) => {
-        return User.find().select('-password');
+        const {
+            perPage = 20,
+            page = 1,
+            sortBy = 'createdAt',
+            order = 'asc',
+            ...filters
+        } = query
+
+        const findObject = {}
+
+        Object.keys(filters).forEach((filterParams) => {
+                switch (filterParams) {
+                    case 'name':
+                        break;
+
+                }
+            }
+        )
+
+        const orderBy = order === 'asc' ? -1 : 1;
+
+        return User.find(findObject)
+            .select('-password')
+            .sort({[sortBy]: orderBy})
+            .limit(+perPage)
+            .skip((page - 1) * perPage);
     }
 }
